a2149359ed4719b7588372577a916ba3ff11345b
shelleyyyyy <shelleywr23@mail.vmi.edu>
2022-03-30 15:08:03 -0400

    asdf

--- src/Page Build Tool/PageBuilder.jsx
+++ src/Page Build Tool/PageBuilder.jsx
@@ -3,22 +3,9 @@ import {retrieveModuleData} from './Module Identification/moduleRetriever'
 import App from '../Pages/App';
 
 export default function PageBuilder({pageTitle}) {
-	
-	const Switch = () => {
-		if(pageTitle == 'App'){
-			return <App></App>
-		} else if(pageTitle == 'Boxes'){
-
-		} else if(pageTitle == 'Settings'){
-
-		} else if(pageTitle == 'Help'){
-
-		} else {
-			return <h1>Page Not Found</h1>
-		}
-	}
-
   return (
-    <Switch></Switch>
+    <h1>
+		{pageTitle}
+	</h1>
   )
 }
