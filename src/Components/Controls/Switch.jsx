f745e2d237d094f1f6a09354b622f47daa4cce1c
BoleBorson <corsonca23@mail.vmi.edu>
2022-04-11 09:40:16 -0400

    adding switch controls

--- src/Components/Controls/Switch.jsx
+++ src/Components/Controls/Switch.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Switch from '@mui/material/Switch';
+import publish from '../../Network/publish'
 
 export default function BasicSwitch({ content }) {
+
+    const [checked, setChecked] = useState(false);
+
+    const handleClick = () => {
+        setChecked(!checked);
+        publish(!checked, 'IOT')
+    }
+
     return (
         <div>
             <h3>
                 {content}
             </h3>
-            <Switch/>
+            <Switch checked={checked} onClick={() => handleClick()}/>
         </div>
     );
 }
--- src/Network/publish.js
+++ src/Network/publish.js
@@ -1,7 +1,6 @@
 export default async function publish(data, queue) {
     let response = await fetch('http://localhost:3001/control/publish', {
         method: 'POST',
-        credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
